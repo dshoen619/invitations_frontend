6687b417e49faf4015f79b14a885eed5e286d1fc
davidshoen <davids@ad-knight.com>
2024-05-29 17:57:06 +0300

    removed names

--- src/App.js
+++ src/App.js
@@ -5,25 +5,11 @@ import { useForm } from "react-cool-form"
 import "./styles.css";
 import invitation_english from './invitation_english.jpg'
 import invitation_hebrew from './invitation_hebrew.jpg'
-
-
-const Field = ({ id, value, label, checked, onChange }) => (
-  <div style={{ marginBottom: '0px' }}>
-    <input id={id} value={value} type="checkbox" checked={checked} onChange={onChange} />
-    <label htmlFor={id}>{label}</label>
-  </div>
-);
-const Group = ({ title, error, children }) => (
-  <div style={{ marginBottom: '20px', textAlign:'center'}}>
-    <h3>{title}</h3>
-    {error && <p style={{ color: 'red' }}>{error}</p>}
-    {children}
-  </div>
-);
+import invitationBackground from './stone-floor-texture.jpg';
 
 function Submitted() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', fontSize: '24px', flexDirection: 'column' }}>
+    <div style={{backgroundImage: `url(${invitationBackground})`, backgroundSize: 'cover', fontFamily:'Garamond, serifra', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', fontSize: '24px', flexDirection: 'column' }}>
       <div>We Got It! Thank You</div>
       <div style={{marginTop:'5%'}}>קיבלנו, תודה</div>
   </div>
@@ -36,21 +22,33 @@ function HomePage() {
   const [checkedItems, setCheckedItems] = useState({});
   const { id } = useParams(); // Grab the id from the URL parameters
   const [data, setData] = useState(['']);
+  const [recordData, setRecordData] = useState({})
   const [comingResponses, setComingResponses] = useState({});
   const [cantComeResponses, setCantComeResponses] = useState({});
+  let [comingCount, setComingCount] = useState(0)
   const navigate = useNavigate()
   
+  function incrementCount() {
+    comingCount = comingCount + 1;
+    setComingCount(comingCount);
+  }
+  function decrementCount() {
+    if (comingCount==0){
+      // do nothing
+    }
+    else{
+      comingCount = comingCount - 1;
+      setComingCount(comingCount);
+    }
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       if (id) {
-        console.log(id)
         try {
           const result = await getData(id);
-          console.log('result', result);
-          console.log(result.fields.names_in_group)
           setData(result.fields.names_in_group.split(',').filter(Boolean));
-          console.log(data)
+          setRecordData(result)
         } catch (error) {
           console.error('Error fetching data:', error);
         }
@@ -60,98 +58,56 @@ function HomePage() {
     fetchData();
   }, [id]); // Add id as a dependency to refetch when it changes
 
-  const handleCheckboxChange = (name, type, value) => {
-    if (type === 'coming') {
-      if (value && cantComeResponses[name]) {
-        setCantComeResponses({
-          ...cantComeResponses,
-          [name]: false,
-        });
-      }
-      setComingResponses({
-        ...comingResponses,
-        [name]: value,
-      });
-    } else if (type === 'cantCome') {
-      if (value && comingResponses[name]) {
-        setComingResponses({
-          ...comingResponses,
-          [name]: false,
-        });
-      }
-      setCantComeResponses({
-        ...cantComeResponses,
-        [name]: value,
-      });
-    }
-  };;
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const uncheckedItems = data.filter(name => !comingResponses[name] && !cantComeResponses[name]);
-    if (uncheckedItems.length > 0) {
-      alert('Please respond for all invitees: ' + uncheckedItems.join(', '));
-    } else {
-      console.log('Form submitted:');
-      console.log('Coming:', comingResponses);
-      console.log("Can't Come:", cantComeResponses);
-      const response = await sendData(comingResponses,cantComeResponses,id)
+    // const uncheckedItems = data.filter(name => !comingResponses[name] && !cantComeResponses[name]);
+    // if (uncheckedItems.length > 0) {
+    //   alert('Please respond for all invitees: ' + uncheckedItems.join(', '));
+    // } else {
+    //   console.log('Form submitted:');
+    //   console.log('Coming:', comingResponses);
+    //   console.log("Can't Come:", cantComeResponses);
+    //   const response = await sendData(comingResponses,cantComeResponses,id)
       navigate('/submitted')
       // Add your form submission logic here
-    }
+    // }
   };
 
   return (
-  <div>
-    <div  style={{ display: 'flex', marginBottom: '10px', justifyContent:'center', marginTop:'10px' }}>
-      <img src={invitation_english} alt="Description of image" style={{ width: '30%', marginRight: '5%' }} />
-      <img src={invitation_hebrew} alt="Description of image" style={{ width: '30%' }} />  
-    </div>
-    <form ref={form} noValidate onSubmit={handleSubmit}>
-    <Group title="Please Let Us Know if You Can Make it :) (:אשמח אם תעדכנו אותנו מי מגיע">
-  <div style={{ width: '90%', margin: '0 auto', justifyContent:'center' }}>
-    <div style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '60px', margin:'0 auto' }}>
-      <div>
-        <div>Name</div>
-        <div> שֵׁם</div>
-      </div>
-      <div>
-        <div>Coming</div>
-        <div>מגיע</div>
-      </div>
-      <div>
-        <div>Can't Come </div>
-        <div>לא מגיע</div>
-      </div>
-    </div>
-    {data.map((name, index) => (
-      <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '5px' }}>
-        <div>{name}</div>
-        <div>
-          <Field
-            id={`coming_${index}`}
-            value="coming"
-            label=""
-            checked={comingResponses[name]}
-            onChange={(event) => handleCheckboxChange(name, 'coming', event.target.checked)}
-          />
-        </div>
-        <div>
-          <Field
-            id={`not_coming_${index}`}
-            value="cantCome"
-            label=""
-            checked={cantComeResponses[name]}
-            onChange={(event) => handleCheckboxChange(name, 'cantCome', event.target.checked)}
-          />
-        </div>
-      </div>
-    ))}
+<div style={{ backgroundImage: `url(${invitationBackground})`, backgroundSize: 'cover', height: '100vh', overflow: 'auto' }}>
+  <div style={{ display: 'flex', marginBottom: '10px', justifyContent:'center', marginTop:'10px' }}>
+    <img src={invitation_english} alt="Description of image" style={{ width: '30%', marginRight: '5%' }} />
+    <img src={invitation_hebrew} alt="Description of image" style={{ width: '30%' }} />  
   </div>
-</Group>
-    <button type="submit">Submit</button>
+  <form ref={form} noValidate onSubmit={handleSubmit}>
+    <div style={{ marginBottom: '20px', textAlign:'center', fontFamily: 'Garamond, serif', fontSize:'25px', width:'100%', maxWidth: '100%' }}>
+      Please Let Us Know if You Can Make it :)
+    </div>
+    <div style={{ marginBottom: '20px', textAlign:'center', fontFamily: 'Garamond, serif', fontSize:'25px', width:'100%', maxWidth: '100%' }}>
+      (:אשמח אם תעדכנו אותנו מי מגיע"
+    </div>
+
+    <div className="App">
+      {recordData && recordData.fields && (
+        <div style={{ fontFamily: 'Garamond, serif', justifyContent: 'center', paddingTop: '8%', textAlign:'center' }}>
+          <div style={{fontSize:'25px'}}>
+            To {recordData.fields.Name}
+          </div>
+          
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent:'center', paddingTop:'8%', paddingBottom:'8%'}}>
+            <button type ="button" className="button-17" onClick={decrementCount}>-</button>
+            <div style={{padding:'5%'}}>{comingCount}</div>
+            <button type ="button" className="button-17" onClick={incrementCount}>+</button>
+          </div>
+        </div>
+      )}
+    </div>
+      
+    <button type="submit" style={{ display: 'block', margin: '0 auto', marginBottom:'5%'}}>Submit</button>
   </form>
-  </div>
+</div>
+
    
 
   );
