9ffe2fdc7bbb1c1f8f2754a501520f885ead85c8
David Shoen <davidshoen@Davids-MacBook-Pro.local>
2024-06-02 17:56:40 +0300

    removed useParams

--- src/homePageEnglish.js
+++ src/homePageEnglish.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useParams, useNavigate } from 'react-router-dom';
 import { getData, sendData } from './api';
 import "./styles.css";
 import invitation_english from './invitation_english.jpg'
